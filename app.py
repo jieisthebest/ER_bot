ae56691427fc2cca86b454b6bfeb7558e03324b4
jieisthebest <83874471+jieisthebest@users.noreply.github.com>
2025-01-13 01:30:49 +0800

    Update app.py
    #Added TODO sql

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect
 from datetime import datetime
 from cs50 import SQL
-import sqlite3
 
-# Connect to the database
-conn = sqlite3.connect('patient.db')
-patient_db = conn.cursor()
-#I am not sure about this
 
 
 # app name
@@ -14,7 +9,6 @@ app = Flask(__name__)
 
 #Datasets
 GENDER = ["Male", "Female", "Other"]
-AGE =["0-17","18-64","65+"]
 SEVERITY=["1","2","3"]
 
 # database configuration
@@ -26,6 +20,30 @@ patient_db = SQL("sqlite:///patient.db")
 def format_date(value, format='%Y-%m-%d %H:%M:%S'):
     return datetime.strptime(value, '%Y-%m-%d %H:%M:%S').strftime(format)
 
+def error(message, code=400):
+    """Render message as an apology to user."""
+
+    def escape(s):
+
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
+            s = s.replace(old, new)
+        return s
+
+    return render_template("error.html", top=code, bottom=escape(message)), code
+
+
+
+
+
 @app.route("/")
 def index():
     """Default Web MD search page"""
@@ -35,9 +53,9 @@ def index():
 @app.route("/triage", methods=["GET", "POST"])
 def triage():
     if request.method == "POST":
-        triage_processor()
+        return triage_processor()
     else:
-        return render_template("triage.html", GENDER=GENDER, SEVERITY=SEVERITY,AGE=AGE)
+        return render_template("triage.html", GENDER=GENDER, SEVERITY=SEVERITY)
 
 def triage_processor():
     name = request.form.get("name")
@@ -45,44 +63,48 @@ def triage_processor():
     gender = request.form.get("gender")
     symptom = request.form.get("symptom")
     severity = request.form.get("severity")
+    # Checks for input
     if not name or not age or not gender or not symptom or not severity:
-        flash(f"Enter all details please!")
+        return redirect("/triage")
+    #check age
+    try:
+        age=int(age)
+
+    except ValueError:
+        return redirect("/triage")
 
     patient_log = symptom
-    # add user to database
-    # TODO check cs50 sql statements do they add up with flow ???? placeholder error
-    patient_db.execute("INSERT INTO patients (name, gender, age, date, patient_log) VALUES (?, ?, ?, CURRENT_TIMESTAMP, ?)", (name, gender, age, patient_log))
-    conn.commit()
-    #grabs ID from the recently created user for patient_id
-    patient_id = patient_db.execute("SELECT last_insert_rowid()").fetchone()[0]
+    print(type(name), type(gender), type(age), type(patient_log))
+    
+    #TODO CS50 sql review error placeholder mismatch 
+    try:
+        patient_db.execute("INSERT INTO patients (name, gender, age, date, patient_log) VALUES (?, ?, ?, CURRENT_TIMESTAMP, ?)", (name, gender, age, patient_log))
+        # grabs ID from the recently created user for patient_id
+        patient_id = patient_db.execute("SELECT last_insert_rowid()").fetchone()[0]
+    except Exception as e:
+
+        return redirect("/triage")
+
+
     # creates array of matched item to table
     results = []
-    # break query of user symptom into words to iterrate and search
+    # break query of user symptom into words to iterate and search
     symptoms = symptom.split()
     for s in symptoms:
         # get the symptom's id
-        subquery = "SELECT id FROM triage WHERE symptom LIKE %s" % ("%" + s + "%")
-        results.extend(triage_db.execute(subquery).fetchall())
+        subquery = "SELECT id FROM triage WHERE symptom LIKE ?"
+        results.extend(triage_db.execute(subquery, ('%' + s + '%',)).fetchall())
 
-        if len(results) <= 0:
-            flash(f"Sorry did not find your symptom{symptom}! The system has logged this case!")
-            # insert a unknown symptom or case at id=0
-            patient_db.execute("INSERT INTO symptom_details (patient_id, symptom_id) VALUES (?,0)", patient_id)
-            # redirect to other ressource to search
-            return redirect("/index")
-        else:
-            for t in results:
-                patient_db.execute("INSERT INTO symptom_details (patient_id, symptom_id) VALUES (?,?)" , patient_id, (t['id']))
+
+    if len(results) <= 0:
+        flash(f"Sorry did not find your symptom {symptom}! The system has logged this case!")
+        # insert an unknown symptom case at id=0
+        patient_db.execute("INSERT INTO symptom_details (patient_id, symptom_id) VALUES (?, 0)", patient_id)
+        # redirect to other resource to search
+        return redirect("/index")
+    else:
+        for t in results:
+            patient_db.execute("INSERT INTO symptom_details (patient_id, symptom_id) VALUES (?, ?)", (patient_id, t['id']))
             #now condition checks
             #location
-            #severity
-
-
-
-
-@app.route("/history", methods=["GET", "POST"])
-def history():
-    """Show history of transactions"""
-    #not sure how to combine 2 tables of various data review SQL
-    #history = patient_db.execute("SELECT  FROM patients WHERE patient_id = ? ORDER BY date DESC", patient_db.execute("SELECT last_insert_rowid()").fetchone()[0])
-    #return render_template("history.html", history=history)
+            #severityfrom flask import Flask, render_template, request, flash, redirect
